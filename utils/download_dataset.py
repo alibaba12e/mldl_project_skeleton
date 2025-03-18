e55d7c31d79886eaaa78e9c2591ddac6e50f6bed
Owais Ali <53053186+alibaba12e@users.noreply.github.com>
2025-03-18 15:45:09 +0100

    Update download_dataset.py

--- utils/download_dataset.py
+++ utils/download_dataset.py
@@ -4,4 +4,4 @@ import subprocess
 subprocess.run(["wget", "http://cs231n.stanford.edu/tiny-imagenet-200.zip"])
 
 # Use unzip to extract the dataset
-subprocess.run(["unzip", "tiny-imagenet-200.zip", "-d", "tiny-imagenet"])
+subprocess.run(["unzip", "tiny-imagenet-200.zip", "-d", "data/tiny-imagenet"])
