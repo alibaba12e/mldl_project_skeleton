aa4482a5fca5bbfc417fe559f9d433919d326ca9
Owais Ali <53053186+alibaba12e@users.noreply.github.com>
2025-03-18 15:20:13 +0100

    Add files via upload

---
+++ utils/download_dataset.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# Use wget to download the dataset
+subprocess.run(["wget", "http://cs231n.stanford.edu/tiny-imagenet-200.zip"])
+
+# Use unzip to extract the dataset
+subprocess.run(["unzip", "tiny-imagenet-200.zip", "-d", "tiny-imagenet"])
